21cc7478b34ddbe306ec99691a85666c3b21a397
Andrew Yip <yipcma@gmail.com>
2016-05-16 21:21:31 +0300

    added stylus package, app layout + componenet setup

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <Link to="/">Reduxstagram</Link>
+      </div>
+    )
+  }
+});
+
+export default Main;
